f83b536ebce57dc486dd3a06db5540346555b5b5
reenboog <alex.gievsky@gmail.com>
2013-11-04 17:47:42 +0700

    Added stackable layers.

--- projects/astroGems/Classes/StackableLayer.h
+++ projects/astroGems/Classes/StackableLayer.h
@@ -9,6 +9,27 @@
 #ifndef astroGems_StackableLayer_h
 #define astroGems_StackableLayer_h
 
+#include "cocos2d.h"
 
+class StackableLayer {
+public:
+    StackableLayer() {
+        baseLayer = nullptr;
+        blackout = nullptr;
+		touchBlocker = nullptr;
+    }
+    
+	virtual void popUp(StackableLayer *baseLayer) = 0;
+	virtual void popOut() = 0;
+	virtual void putOn(StackableLayer *overlay) = 0;
+	virtual void takeOff() = 0;
+    
+	virtual void disableTouches() = 0;
+	virtual void enableTouches() = 0;
+protected:
+	cocos2d::LayerColor *blackout;
+	StackableLayer *baseLayer;
+	cocos2d::Menu *touchBlocker;
+};
 
 #endif
